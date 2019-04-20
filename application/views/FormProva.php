5cd5857628b34b994af3157634d6330bee24ee45
Jaíne <viannajaine@gmail.com>
2019-04-20 11:53:33 -0300

    alterações bootstrap

--- application/views/FormProva.php
+++ application/views/FormProva.php
@@ -2,44 +2,58 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title> Cadastro de Provas </title>
+        <title> Formulário de Prova </title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     </head>
     <body>
+        <nav  class="navbar navbar-expand-lg navbar-light bg-success">
+            <a class="navbar-brand text-white" href="#"><i class="far fa-list-alt"></i> Formulário de Prova </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="ListaProvas.php"> Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link active" href="http://127.0.0.1/GincanaCI/index.php/views/FormProva.php"> Lista de Provas </a>
+                </div>
+            </div>
+        </nav>
         <?php
         $mensagem = $this->session->flashdata('mensagem');
         if (isset($mensagem)) {
             echo $mensagem;
         }
         ?>
-        <div class="shadow rounded card" style="width: 20rem; margin-top: 80px; margin-left: 39%;">
-            <div class="card-header bg-success" style="color: white;">Fomulário Prova</div>
-            <form action="" method="POST">
-                <div class="form-row">
-                    <input type="hidden" name="id" value="<?= (isset($prova)) ? $prova->id : ''; ?>">
-                    <div class="col-md-11 mb-3">
-                        <label for="nome" style="margin-left: 10px; margin-top: 10px"> Nome: </label>
-                        <input type="text" name="nome" id="nome" class="form-control" value="<?= (isset($prova)) ? $prova->nome : ''; ?>" style="margin-left: 12px;">
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <form action="" method="POST" class="shadow p-3 mb-5 bg-white rounded" style="margin-top: 10px;">
+                    <div class="form-row">
+                        <input type="hidden" name="id" value="<?= (isset($prova)) ? $prova->id : ''; ?>">
+                        <div class="col-md-11 mb-3">
+                            <label for="nome" style="margin-left: 10px; margin-top: 10px"> Nome: </label>
+                            <input type="text" name="nome" id="nome" class="form-control" value="<?= (isset($prova)) ? $prova->nome : ''; ?>" style="margin-left: 12px;">
+                        </div>
+                        <div class="col-md-11 mb-3">
+                            <label for="tempo" style="margin-left: 10px;"> Tempo: </label>
+                            <input type="text" name="tempo" id="tempo" class="form-control" value="<?= (isset($prova)) ? $prova->tempo : ''; ?>" style="margin-left: 12px;">
+                        </div>
+                        <div class="col-md-11 mb-3">
+                            <label for="descricao" style="margin-left: 10px;"> Descrição: </label>
+                            <input type="text" name="descricao" id="descricao" class="form-control" value="<?= (isset($prova)) ? $prova->descricao : ''; ?>" style="margin-left: 12px;">
+                        </div>
+                        <div class="col-md-11 mb-3">
+                            <label for="nintegrantes" style="margin-left: 10px;"> N°Integrantes: </label>
+                            <input type="text" name="nintegrantes" id="nintegrantes" class="form-control" value="<?= (isset($prova)) ? $prova->nintegrantes : ''; ?>" style="margin-left: 12px;">
+                        </div>
                     </div>
-                    <div class="col-md-11 mb-3">
-                        <label for="tempo" style="margin-left: 10px;"> Tempo: </label>
-                        <input type="text" name="tempo" id="tempo" class="form-control" value="<?= (isset($prova)) ? $prova->tempo : ''; ?>" style="margin-left: 12px;">
+                    <hr>
+                    <div style="text-align: center; margin-bottom: 10px;">
+                        <button class="btn btn-warning" type="submit" value="Submit"><i class="far fa-share-square"></i> Enviar </button>
+                        <button type="button" class="btn btn-outline-danger" value="Reset"><i class="fas fa-ban"></i> Limpar </button>
                     </div>
-                    <div class="col-md-11 mb-3">
-                        <label for="descricao" style="margin-left: 10px;"> Descrição: </label>
-                        <input type="text" name="descricao" id="descricao" class="form-control" value="<?= (isset($prova)) ? $prova->descricao : ''; ?>" style="margin-left: 12px;">
-                    </div>
-                    <div class="col-md-11 mb-3">
-                        <label for="nintegrantes" style="margin-left: 10px;"> N°Integrantes: </label>
-                        <input type="text" name="nintegrantes" id="nintegrantes" class="form-control" value="<?= (isset($prova)) ? $prova->nintegrantes : ''; ?>" style="margin-left: 12px;">
-                    </div>
-                </div>
-                <hr>
-                <div style="text-align: center; margin-bottom: 10px;">
-                <button class="btn btn-primary" type="submit" value="Submit"> Enviar </button>
-                <button type="button" class="btn btn-outline-success" value="Reset"> Limpar </button>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </body>
 </html>
--- application/views/ListaProvas.php
+++ application/views/ListaProvas.php
@@ -4,43 +4,59 @@
         <meta charset="UTF-8">
         <title> Lista de Provas </title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     </head>
     <body>
-        <h1> Lista de Provas </h1>
+        <nav  class="navbar navbar-expand-lg navbar-light bg-success">
+            <a class="navbar-brand text-white" href="#"><i class="far fa-list-alt"></i> Lista de Provas </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="ListaProvas.php"> Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link active" href="http://127.0.0.1/GincanaCI/index.php/views/FormProva.php"> Formulário Prova </a>
+                </div>
+            </div>
+        </nav>
         <?php
         $mensagem = $this->session->flashdata('mensagem');
         if (isset($mensagem)) {
             echo $mensagem;
         }
         ?>
-        <table border="1" class="table">
-            <thead class="bg-success" style="color: white;">
-                <tr>
-                    <th scope="col"> Nome </th>
-                    <th scope="col"> Tempo </th>
-                    <th scope="col"> Descrição </th>
-                    <th scope="col"> N°Integrantes </th> 
-                    <th scope="col"> Ações </th>
-                </tr>     
-            </thead>
-            <tbody>
-                <?php
-                foreach ($provas as $key => $p) {
-                    echo '<tr>';
-                    echo '<td>' . $p->nome . '</td>';
-                    echo '<td>' . $p->tempo . '</td>';
-                    echo '<td>' . $p->descricao . '</td>';
-                    echo '<td>' . $p->nintegrantes . '</td>';
-                    echo '<td>'
-                    . '<a href="' . $this->config->base_url() . '/index.php/Prova/alterar/' . $p->id . '" class="btn btn-warning"> Alterar </a>'
-                    . '<a href="' . $this->config->base_url() . '/index.php/Prova/deletar/' . $p->id . '" class="btn btn-outline-danger"> Deletar </a></td>';
+        <div class="container" >
+            <div class="row justify-content-md-center">
+                <table border="1" class="table shadow p-3 mb-5 bg-white rounded" style="margin-top: 10px;">
+                    <thead  class="thead-dark">
+                        <tr>
+                            <th scope="col"> Nome </th>
+                            <th scope="col"> Tempo </th>
+                            <th scope="col"> Descrição </th>
+                            <th scope="col"> N°Integrantes </th> 
+                            <th scope="col"> Ações </th>
+                        </tr>     
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($provas as $key => $p) {
+                            echo '<tr>';
+                            echo '<td scope="row">' . $p->nome . '</td>';
+                            echo '<td scope="row">' . $p->tempo . '</td>';
+                            echo '<td scope="row">' . $p->descricao . '</td>';
+                            echo '<td scope="row">' . $p->nintegrantes . '</td>';
+                            echo '<td scope="row" style="text-align: center;">'
+                            . '<a href="' . $this->config->base_url() . '/index.php/Prova/alterar/' . $p->id . '" class="btn btn-warning" style="margin-right:4px;"><i class="far fa-edit"></i> Alterar </a>'
+                            . '<a href="' . $this->config->base_url() . '/index.php/Prova/deletar/' . $p->id . '" class="btn btn-outline-danger"><i class="far fa-trash-alt"></i> Deletar </a></td>';
 
-                    echo '</tr>';
-                }
-                ?>
+                            echo '</tr>';
+                        }
+                        ?>
 
-            </tbody>
+                    </tbody>
 
-        </table>
+                </table>
+            </div>
+        </div>
     </body>
 </html>
